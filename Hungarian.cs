dd8ae81fe0a908a6d7cdfa5ceec81e9c1d32e5e4
Builderb <builderbgamer@gmail.com>
2018-02-07 17:27:09 +0000

    Update Hungarian.cs

--- Hungarian.cs
+++ Hungarian.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using static Bot.Translations._Trans;
 
 namespace Bot.Translations
@@ -9,19 +9,19 @@ namespace Bot.Translations
         {
             Error = new TError
             {
-                NoEmbedPerms = "A Bot engedélyt kér \ "Linkek beágyazása \"",
+                NoEmbedPerms = "A Bot engedélyt kér \"Linkek beágyazása\"",
                 AdminOnly = "Ön nem szerver admin",
                 PlayerNotFound = "A (z) {0} játékos nem található",
                 ApiError = "API hiba",
-                UnknownItemID = "Nem található elem név vagy azonosító",
-                UnknownMob = "Nem találja meg a mob nevéte",
                 EnterIP = "Írjon be IP-címet",
                 InvalidIP = "Érvénytelen IP",
                 Cooldown = "Ön 1 percig tartja a cooldownot!",
                 EnableQuery = "Az Minecraft kiszolgáló nem rendelkezik engedélyezési lekérdezéssel a server.properties mappában",
                 ListNoServers = "Ennek a közösségnek nincs felsorolt szervere",
                 UnknownArg = "Ismeretlen érvelés",
-                TextLimit = "A szöveg legfeljebb 22 betű / szám lehet"
+                TextLimit = "A szöveg legfeljebb 22 betű / szám lehet",
+                RequireAttachFiles = "Botnak csatolni kell a fájlok engedélyezését",
+                UnknownWiki = "Ismeretlen wiki tétel"
             },
 
             Main = new TMain
@@ -31,24 +31,24 @@ namespace Bot.Translations
                     "[ mc/bot ]( Bot Invite/Info/Statisztika/Linkek )",
                     "[ mc/quiz ]( Minecraft Kviz :D )",
                     "[ mc/colors ]( MC Szinkodok )",
-                    "[ mc/uuid (Player) ]( Player UUID )",
+                    "[ mc/uuid (játékos) ]( játékos UUID )",
                     "[ mc/ping (IP) ]( Ping egy kiszolgalot )",
                     "[ mc/list ]( Listazza a kozossegi szervereket )",
                     "[ mc/wiki ]( Wiki az elemek/mobok/varazslok/bajitalok szamara )",
                     "[ mc/version ]( Minecraft verzloinformaciok )",
-                    "[ mc/skin (Player) ]( Jatekos bor )",
-                    "[ mc/names (Player) ]( MC fioknev elozmemyei )",
+                    "[ mc/skin ( játékos ) ]( Jatekos bor )",
+                    "[ mc/names ( játékos ) ]( MC fioknev elozmemyei )",
                     "[ mc/status ]( Mojang allapot )",
                     "[ mc/get (Text) ]( Kapjon egy eredmenyt )",
-                    "[ mc/minime (Player) ]( Minify player skin )",
+                    "[ mc/minime ( játékos ) ]( Minify player skin )",
                     "[ mc/playing ]( Az Minecraft jatekosait )",
-                    "[ mc/hypixel (Player) ]( Hypixel player stats )",
+                    "[ mc/hypixel ( játékos ) ]( Hypixel player stats )",
                     "[ mc/admin ]( Kozossegi adminisztracios parancsok )",
                     "[ mc/invite ]( Szerezd meg a flut )"
                 },
                 HelpFooter = "Vannak rejtett parancsok is;)",
                 MultiMC = "A MultiMC lehetővé teszi több verzió kezelését és elindítását könnyű forge / mods telepítéssel",
-                SkinEditor = "Online bőrszerkesztő"              
+                SkinEditor = "Online bőrszerkesztő",
                 PleaseWait = "Kérem várjon, míg én ping",
                 ServerAdminUse = "A szerver adminisztrátornak kell használni",
                 SkinArgs = "(Lejátszó) | fej | kocka teljes | lop",
@@ -81,6 +81,16 @@ namespace Bot.Translations
                 Width = "Szélesség",
             },
 
+            Profile = new TProfile
+            {
+                Badges = "Jelvénys",
+                BadgeInfo = "Kitűző információk",
+                InvalidUserID = "Érvénytelen felhasználói azonosító",
+                UnknownUser = "Nem sikerült megtalálni a felhasználót",
+                NotInServer = "Nem vagy a szerveren",
+                SetMCName = "A Minecraft fiók használatának beállítása"
+            },
+
             Admin = new TAdmin
             {
                 Commands = new List<string>
@@ -100,9 +110,9 @@ namespace Bot.Translations
                 DelServerEnter = "Szerver törlése a következővel:",
                 DelServerNone = "Ez a kiszolgáló nem a list",
                 DelServerDeleted = "Eltávolította a {0} kiszolgálót a céhlistából",
-                PrefixReset = "A kiszolgáló előtagjának visszaállítása mc /"
+                PrefixReset = "A kiszolgáló előtagjának visszaállítása mc /",
+                LanguageSet = "Közösségi nyelv"
             }
-
         };
     }
 }
