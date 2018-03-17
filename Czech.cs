91392d92aa405d568f4403b98584ca728ab517e3
renzotom <renzotominclubpenguin@gmail.com>
2018-03-17 18:17:55 +0100

    First version of Czech language

--- Czech.cs
+++ Czech.cs
@@ -9,106 +9,106 @@ namespace Bot.Translations
         {
             Error = new TError
             {
-                NoEmbedPerms = "Bot requires permission \" Embed Links \"",
-                AdminOnly = "You are not a server admin",
-                PlayerNotFound = "Player {0} not found",
-                ApiError = "API Error",
-                EnterIP = "Enter an IP",
-                InvalidIP = "Invalid IP",
-                Cooldown = "You are on cooldown for 1 mins!",
-                EnableQuery = "Minecraft server does not have enable-query set in server.properties",
-                ListNoServers = "This community has no servers listed",
-                UnknownArg = "Unknown argument do",
-                TextLimit = "Text cannot be more than 22 letters/numbers",
-                RequireAttachFiles = "Bot requires attach files permission",
-                UnknownWiki = "Unknown wiki item"
+                NoEmbedPerms = "Bot vyžaduje oprávnění \" Embed Links \"",
+                AdminOnly = "Nejste administrátorem serveru!",
+                PlayerNotFound = "Hráč {0} nebyl nalezen",
+                ApiError = "Chyba API",
+                EnterIP = "Zadejte IP",
+                InvalidIP = "Neplatná IP",
+                Cooldown = "Musíte počkat 1 minutu!",
+                EnableQuery = "Minecraft server nemá v „server.properties“ nastavené „enable-query“",
+                ListNoServers = "Tato komunita nemá uvedena žádný server",
+                UnknownArg = "Neznámý argument",
+                TextLimit = "Text nemůže být delší jak 22 písmen či čísel",
+                RequireAttachFiles = "Bot potřebuje oprávnění ",
+                UnknownWiki = "Neznámý předmět wikipedie"
             },
 
             MainCommands = new TMainCommands
             {
-                BotInfo = "Bot info and stats",
-                Quiz = "Minecraft quiz",
-                Color = "Minecraft color codes",
-                NameHistory = "name history",
-                UserProfile = "Discord user profile",
-                Ping = "Ping a Minecraft server",
-                Version = "Minecraft version info",
-                Rcon = "Remote console for your Minecraft server",
-                Achievement = "Get an achievement",
-                Playing = "People playing Minecraft",
-                List = "List the community servers",
-                Admin = "Community admin commands",
-                Invite = "Add bot to your Discord server"
+                BotInfo = "Informace a statistiky bota",
+                Quiz = "Kvíz z Minecraftu",
+                Color = "Barevné kódy v Minecraftu",
+                NameHistory = "historie jmen",
+                UserProfile = "Uživatelský profil na Discordu",
+                Ping = "Ping Minecraft serveru",
+                Version = "Informace o Minecraft verzích",
+                Rcon = "Vzdálená konzole pro Váš Minecraft server",
+                Achievement = "Získat úspěch",
+                Playing = "Lidé hrající Minecraft",
+                List = "Seznam serverů komunity",
+                Admin = "Příkazy admina komunity",
+                Invite = "Přidat bota na Váš Discord server"
             },
 
             Main = new TMain
             {
-                HelpFooter = "There are some hidden commands aswell ;)",
-                MultiMC = "MultiMC allows you to manage and launch multiple versions with easy forge/mods installation",
-                PleaseWait = "Please wait while I ping",
-                ServerAdminUse = "Server admin should use",
-                StoleSkin = "Stole a skin",
-                NameOnlyOne = "Player {0} only has 1 name on records",
-                PlayingMinecraft = "People playing Minecraft",
-                Hi = "Hi",
-                BotDesc = "If you have any issue, suggestions or language translations please contact me",
-                First = "First"
+                HelpFooter = "Nějaké příkazy jsou samozřejmě skryté ;)",
+                MultiMC = "MultiMC Vám umožní spravovat a spustit více verzí s lehkou instalací forge/módů",
+                PleaseWait = "Počkejte, než získám ping",
+                ServerAdminUse = "Admin serveru by měl používat",
+                StoleSkin = "Ukrást skin",
+                NameOnlyOne = "Hráč {0} má v záznamech pouze 1 jméno",
+                PlayingMinecraft = "Lidé hrající Minecraft",
+                Hi = "Ahoj",
+                BotDesc = "Pokud máte nějaké problémy, nápady či návrhy k překladu, kontaktujte mě",
+                First = "První"
             },
 
             Hidden = new THidden
             {
-                FoundHiddenCommand = "Hey, you found a secret command :D",
-                Herobrine = "Always watching you...",
-                Notch = "Minecraft was created by Notch aka Markus Persson"
+                FoundHiddenCommand = "Hej, našel jsi tajný příkaz :D",
+                Herobrine = "Stálě tě sleduji...",
+                Notch = "Minecraf byl vytvořen hráčem Notch aka Markus Persson"
             },
 
             Wiki = new TWiki
             {
-                Blocks = "Blocks",
-                Unknown = "Unknown",
-                Player = "Player",
-                Players = "Players",
-                Attack = "Attack",
-                Easy = "Easy",
-                Hard = "Hard",
-                Health = "Health",
-                Height = "Height",
-                Width = "Width"
+                Blocks = "Bloky",
+                Unknown = "Neznámé",
+                Player = "Hráč",
+                Players = "Hráči",
+                Attack = "Útok",
+                Easy = "Lehká",
+                Hard = "Těžká",
+                Health = "Zdraví",
+                Height = "Výška",
+                Width = "Šířka"
             },
 
             Profile = new TProfile
             {
-                Badges = "Badges",
-                BadgeInfo = "Badge information",
-                InvalidUserID = "Invalid user id",
-                UnknownUser = "Could not find user",
-                NotInServer = "You are not on the server",
-                SetMCName = "To set your Minecraft account use"
+                Badges = "Odznaky",
+                BadgeInfo = "Informace o odznaku",
+                InvalidUserID = "Neznámé ID uživatele",
+                UnknownUser = "Uživatele se nepodařilo nalézt",
+                NotInServer = "Nejste na serveru",
+                SetMCName = "Pro nastavení Vašeho Minecraft účtu použijte"
             },
 
             AdminCommands = new TAdminCommands
             {
-                MCRole = "Create a Minecraft role for verified users",
-                AutoPost = "Auto post your Minecraft server status",
-                AddServer = "Add a Minecraft server to the community list",
-                RemoveServer = "Remove a Minecraft server from the community list",
-                SetPrefix = "Set a custom prefix for the bot",
-                ResetPrefix = "Remove the custom prefix"
+                MCRole = "Vytvořte Minecraft roli pro ověřené uživatele",
+                AutoPost = "Automaticky informovat o stavu Vašeho Minecraft serveru",
+                AddServer = "Přidat Minecraft server do komunitního seznamu",
+                RemoveServer = "Odstranit Minecraft server z komunitního seznamu",
+                SetPrefix = "Nastavit botovi vlastní prefix",
+                ResetPrefix = "Odstranit vlastní prefix"
             },
 
             Admin = new TAdmin
             {
-                WantTranslation = "Want a language translated? Contact",
-                ChangeLang = "Change Community Language",
-                UseList = "Use mc/list for a list of this guilds minecraft servers",
-                AddServer = "Enter a tag, ip and name",
-                AddServerAdded = "Added server {0} to the guild list",
-                AddServerAlready = "This server is already on the list",
-                DelServerEnter = "Delete a server with",
-                DelServerNone = "This server is not on the list",
-                DelServerDeleted = "Removed server {0} from the guild list",
-                PrefixReset = "Server prefix reset use mc/",
-                LanguageSet = "Community language set"
+                WantTranslation = "Chcete bota přeložit? Kontaktujte",
+                ChangeLang = "Změnit jazyk komunity",
+                UseList = "Použijte mc/list pro seznam doporučených Minecraft serverů",
+                AddServer = "Zadejte tag, ip a název",
+                AddServerAdded = "Server {0} přidán do seznamu",
+                AddServerAlready = "Tento server již je v seznamu",
+                DelServerEnter = "Odstranit server s",
+                DelServerNone = "Tento server není v seznamu",
+                DelServerDeleted = "Server {0} odstraněn ze seznamu",
+                PrefixReset = "Prefix serveru resetujete pomocí mc/",
+                LanguageSet = "Jazyk komunity nastaven na"
             }
         };
     }
