c4041a03d16957a15079447a3699b306b47b41d7
MrZillaGold <42776347+MrZillaGold@users.noreply.github.com>
2018-09-23 16:12:30 +1000

    Russian.cs

--- Minecraft-New/Russian.cs
+++ Minecraft-New/Russian.cs
@@ -8,179 +8,179 @@ namespace Bot.Translations
         {
             Error = new TError
             {
-                APIError = "API Error",
-                NoList = "This community has no servers listed",
-                NotVoted = "You have not voted for the bot",
-                PlayerNotFound = "Player not found",
-                NoAttachFiles = "Bot does not have permission to Attach Files"
+                APIError = "Ошибка в API",
+                NoList = "В этом Discord сервере, не добавлены сервера Minecraft",
+                NotVoted = "Вы не проголосовали за бота",
+                PlayerNotFound = "Игрок не найден",
+                NoAttachFiles = "Бот не имеет прав на прикрепление файлов"
             },
 
             Misc = new TMisc
             {
-                Stats = "Stats",
-                Software = "Software",
-                List = "List",
-                Yes = "Yes",
-                No = "No",
-                Server = "Server",
-                Status = "Status",
-                Connected = "Connected",
-                Blocks = "Blocks",
-                Unknown = "Unknown",
-                Player = "Player",
-                Players = "Players",
-                Attack = "Attack",
-                Easy = "Easy",
-                Hard = "Hard",
-                Health = "Health",
-                Height = "Height",
-                Width = "Width",
-                Download = "Download"
+                Stats = "Статистика",
+                Software = "Программа",
+                List = "Список",
+                Yes = "Да",
+                No = "Нет",
+                Server = "Сервер",
+                Status = "Статус",
+                Connected = "Подключение",
+                Blocks = "Блоки",
+                Unknown = "Неизвестно",
+                Player = "Игрок",
+                Players = "Игроки",
+                Attack = "Атака",
+                Easy = "Легкий",
+                Hard = "Сложный",
+                Health = "Здоровье",
+                Height = "Высота",
+                Width = "Ширина",
+                Download = "Скачать"
             },
 
             MainCommands = new TMainCommands
             {
-                UserCmd = "User profile",
-                VerifyCmd = "Verify your Minecraft account",
-                BotCmd = "Bot information",
-                ColorCmd = "Minecraft color codes",
-                QuizCmd = "Minecraft quiz",
-                ListCmd = "Community servers",
-                UUIDCmd = "Get a players UUID",
-                SkinCmd = "Get a players skin",
-                NamesCmd = "Get a players name history",
-                GetCmd = "Get a Minecraft achievement",
-                ChangelogCmd = "Latest Minecraft version",
-                GalleryCmd = "Banners and artwork",
-                VersionCmd = "Version information",
-                RconCmd = "Remote console for your Minecraft server",
-                WikiCmd = "Full Minecraft guide",
-                AdminCmd = "Discord admin commands",
-                InviteCmd = "Invite the bot",
-                Footer = "There are some hidden commands aswell",
-                MultiMC = "allows you to manage and load diffrent Minecraft versions with easy mod installation"
+                UserCmd = "Профиль пользователя",
+                VerifyCmd = "Подтвердите свой профиль",
+                BotCmd = "Информация о боте",
+                ColorCmd = "Цветовые кода Minecraft",
+                QuizCmd = "Minecraft викторина",
+                ListCmd = "Список серверов",
+                UUIDCmd = "Информация о игроке по UUID",
+                SkinCmd = "Получить скин игрока",
+                NamesCmd = "Получить информацию о смене никнеймов",
+                GetCmd = "Сгенерировать достижение Minecraft",
+                ChangelogCmd = "Изменения в Minecraft",
+                GalleryCmd = "Баннеры и картинки",
+                VersionCmd = "Информация о версии Minecraft",
+                RconCmd = "Удаленная консоль для вашего Minecraft сервера",
+                WikiCmd = "Minecraft wiki",
+                AdminCmd = "Команды для настройки бота",
+                InviteCmd = "Пригласать бота на свой сервер",
+                Footer = "Также есть несколько секретных команд",
+                MultiMC = "позволяет управлять и загружать новые версии Minecraft, а также легко устанавливать моды"
             },
 
             Main = new TMain
             {
-                HaveIssues = "If you have any issues please contact me on Discord",
-                playerstats = "player stats",
-                GetStats = "Get player stats from",
-                PlayingInfo = "Users playing Minecraft",
-                WantListed = "Want your Minecraft server listed here? use"
+                HaveIssues = "Если у вас есть вопросы свяжитесь со мной в Discord",
+                playerstats = "статистика игрока",
+                GetStats = "Получить статистику игрока на",
+                PlayingInfo = "Количество игроков в Minecraft",
+                WantListed = "Хотите, чтобы ваш сервер Minecraft был здесь? Используйте"
             },
 
             Gallery = new TGallery
             {
-                BannerRandom = "Get a random banner",
-                BannerName = "Get a banner by name",
-                ArtRandom = "Get a random artwork",
-                WallpaperRandom = "Get a random wallpaper"
+                BannerRandom = "Получить рандомный баннер",
+                BannerName = "Получить баннер по имени",
+                ArtRandom = "Получить рандомную картинку",
+                WallpaperRandom = "Получить рандомные обои"
             },
 
             Ping = new TPing
             {
-                Java = "Ping a Minecraft server",
-                PE = "Ping a pocket edition server",
-                Full = "Ping a Minecraft server with player list",
-                Banner = "Banner version of ping",
-                EnterIP = "Enter an IP address",
-                Wait = "Please wait while i ping",
-                NoEnableQuery = "Server does not have enable-query set in server.properties",
-                InvalidIP = "Invalid IP address",
-                InvalidPort = "Invalid server port",
-                OnCooldown = "You are on cooldown for 1 minute",
-                NoPlayers = "No players online"
+                Java = "Задержка Minecraft сервера",
+                PE = "Задержка Pocket Edition сервера",
+                Full = "Задержка Minecraft сервера и список игроков играющие на нём",
+                Banner = "Баннерная версия задержки",
+                EnterIP = "Укажите IP адрес сервера",
+                Wait = "Подождите, сбор информации о задержке",
+                NoEnableQuery = "На сервере не включен пераметр enable-query, включите его в server.properties",
+                InvalidIP = "Неверный IP адрес",
+                InvalidPort = "Неверный порт",
+                OnCooldown = "Частое выполнение команд! Подожите 1 минуту",
+                NoPlayers = "Нет игроков онлайн"
             },
 
             Hidden = new THidden
             {
-                FoundHiddenCommand = "Hey, you found a secret command :D",
-                Herobrine = "Always watching you...",
-                Notch = "Minecraft was created by Notch aka Markus Persson"
+                FoundHiddenCommand = "Ты нашел секретную команду :D",
+                Herobrine = "Всегда смотрит за тобой...",
+                Notch = "Minecraft создан Notch aka Марку Перссон"
             },
 
             Rcon = new TRcon
             {
-                Title = "Remote console",
-                Info = "Allows Minecraft server owners to run commands from Discord with no plugins or mods needed",
-                NotSetup = "Remote console has not been setup",
-                Execute = "Run console commands using",
+                Title = "Удаленная консоль",
+                Info = "Позволяет использовать команды на сервере Minecraft прямо из Discord",
+                NotSetup = "Удаленная консоль не подключена",
+                Execute = "Запускайте команды на сервере используя",
 
-                ConfigCmd = "View the config",
-                BlacklistCmd = "Blacklist commands for owners only",
-                AccessCmd = "Give other users access",
-                EnableCmd = "Enable remote console",
-                DisableCmd = "Disable remote console",
-                FixCmd = "Attempt to fix the connection",
-                ResetCmd = "Delete the remote console config",
+                ConfigCmd = "Посмотреть настройки",
+                BlacklistCmd = "Команды занесенные в чёрный список доступны только владельцу сервера",
+                AccessCmd = "Дать доступ другим пользователям",
+                EnableCmd = "Включить удаленную консоль",
+                DisableCmd = "Выключить удаленную консоль",
+                FixCmd = "Попытка установить соединение",
+                ResetCmd = "Удалить настройки удаленной консоли",
 
-                AccessList = "Access list",
-                AccessOwner = "Full access to commands and config",
-                AccessAdmin = "Access to commands that are not blacklisted",
-                AccessMod = "Access to ban and kick commands only",
-                CommandBlacklist = "Command blacklist",
-                BlacklistInfo = "Only owners can use blacklisted commands",
-                Enabled = "Remote console has been enabled",
-                Disabled = "Remote console has been disabled ",
-                FixAttempt = "Attempting to fix remote console connection",
-                ResetConfirm = "To confirm reset please use",
-                Reset = "Remote console config has been deleted"
+                AccessList = "Список доступа",
+                AccessOwner = "Комманды для полного доступа и настройки",
+                AccessAdmin = "Доступ к командам не занесённые в чёрный список",
+                AccessMod = "Доступ только к коммандам kick и ban",
+                CommandBlacklist = "Команды занесенные в черный список",
+                BlacklistInfo = "Только владелец может использовать команды занесенные в черный список",
+                Enabled = "Удаленная консоль включена",
+                Disabled = "Удаленная консоль выключена",
+                FixAttempt = "Переподключение к удаленной консоли",
+                ResetConfirm = "Чтобы подтвердить сброс, используйте",
+                Reset = "Настройки удаленной консоли удалены"
             },
 
             Badges = new TBadges
             {
-                Owner = "Owner of the bot",
-                MobHunter = "Expert knowledge of monsters",
-                Translator = "Helped translate the bot",
-                Verified = "Has a verified Minecraft account",
-                Voted = "Voted for the bot",
-                PlayedOn = "Played on"
+                Owner = "Владелец бота",
+                MobHunter = "Эксперт по знаниям в мобах",
+                Translator = "Помощь в переводе бота",
+                Verified = "Имеет лицензионный аккаунт Minecraft",
+                Voted = "Проголосовал за бота",
+                PlayedOn = "Играл на"
             },
 
             User = new TUser
             {
-                NotVerified = "This user has no verified Minecraft account use",
-                LastPlayed = "Last played;",
-                Badges = "Badges",
-                BadgesInfo = "View badges with",
-                NoBadges = "You have no badges"
+                NotVerified = "Этот пользователь не имеет лицензионного аккаунта Minecraft, для привязки используйте",
+                LastPlayed = "В последний раз играл на;",
+                Badges = "Значки",
+                BadgesInfo = "Информация о значках",
+                NoBadges = "У вас нет значков"
             },
 
             Verify = new TVerify
             {
-                Title = "Minecraft account verification",
-                Info = "Verify your Minecraft account for cool features and roles",
-                Step1 = "Load Minecraft 1.12.2",
-                Step2 = "Join this server",
-                Step3 = "Type this in Discord"
+                Title = "Привязка аккаунта Minecraft",
+                Info = "Привяжите свой лицензионный аккаунт Minecraft",
+                Step1 = "Загружите Minecraft 1.12.2",
+                Step2 = "Зайдите на сервер",
+                Step3 = "Введите в Discord"
             },
 
             AdminCommands = new TAdminCommands
             {
-               AddCmd = "Add a Minecraft server to the list",
-               AutopostCmd = "Auto post your Minecraft server status",
-               DelCmd = "Remove a Minecraft server from the list",
-               LangCmd = "Change community language",
-               ResetPrefixCmd = "Remove the custom prefix",
-               RoleCmd = "Create a Minecraft role for verified users",
-               SetPrefixCmd = "Set a custom prefix"
+               AddCmd = "Добавить Minecraft сервер в список",
+               AutopostCmd = "Автоматическая отправка информации о сервере",
+               DelCmd = "Удалить Minecraft сервер из списка",
+               LangCmd = "Изменить язык",
+               ResetPrefixCmd = "Сбросить префикс бота",
+               RoleCmd = "Создать Minecraft роль для подтвержденных пользователей",
+               SetPrefixCmd = "Установить кастомный префикс"
             },
 
             Admin = new TAdmin
             {
-               NotAdmin = "You are not a server admin",
-               LangSet = "Community language set to",
-               PrefixSet = "Custom prefix set to",
-               ChooseToRemove = "Choose a number to remove a server",
-               EnterName = "Enter a name for the server",
-               WantToAdd = "Would you like to add a server? yes/no",
-               GlobalList = "Apply here to get your Minecraft server listed publicly",
-               PrefixReset = "Custom prefix removed use",
-               AlreadyAdded = "This server is already listed on",
-               ServerAdded = "Server has been added to",
-               ServerRemoved = "Server has been removed from",
-               WantTranslated = "Want a language translated? contact me on Discord"
+               NotAdmin = "Вы не администратор сервера",
+               LangSet = "Язык установлен на",
+               PrefixSet = "Кастомный префикс утсановлен на",
+               ChooseToRemove = "Выберите номер сервера для удаления",
+               EnterName = "Укажите имя сервера",
+               WantToAdd = "Вы хотите добавить сервер? да/нет",
+               GlobalList = "Укажите здесь, хотите ли вы чтобы сервер Minecraft был в публичном доступе",
+               PrefixReset = "Кастомный префикс удален, испрользуйте",
+               AlreadyAdded = "Этот сервер уже есть в списке",
+               ServerAdded = "Сервер был добавлен",
+               ServerRemoved = "Сервер удален",
+               WantTranslated = "Хотиет помочь в переводе? Свяжитесь со мной в Discord"
             }
         };
     }
